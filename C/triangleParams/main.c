ba3b6a2086330dc58090a599508ece165837f8cb
Droxus <alexredov@gmail.com>
2023-09-11 16:24:33 +0300

    triangleParams: refactoring

--- C/triangleParams/main.c
+++ C/triangleParams/main.c
@@ -1,5 +1,7 @@
 #include<stdio.h>
 #include<math.h>
+#include<stdbool.h>
+#include<float.h> 
 
 struct object {
     const char key;
@@ -7,24 +9,19 @@ struct object {
 };
 
 float getTrianglePerimeter(float a, float b, float c) {
-    float perimeter = a + b + c;
-    return perimeter;
+    return a + b + c;
 }
-float getTriangleArea(float a, float b, float c, float p) {
-    float area = sqrt(p * (p - a) * (p - b) * (p - c));
-    return area;
+float getTriangleArea(float a, float b, float c, float semiPerimeter) {
+    return sqrt(semiPerimeter * (semiPerimeter - a) * (semiPerimeter - b) * (semiPerimeter - c));
 }
 float getTriangleHeight(float area, float side) {
-    float height = 2 * area / side;
-    return height;
+    return 2 * area / side;
 }
 float getTriangleMedian(float a, float b, float c) {
-    float median = 0.5 * sqrt(2 * pow(b, 2) + 2 * pow(c, 2) - pow(a, 2));
-    return median;
+    return 0.5 * sqrt(2 * pow(b, 2) + 2 * pow(c, 2) - pow(a, 2));
 }
-float getTriangleBisection(float a, float b, float c, float p) {
-    float bisection = (2 / (b + c)) * sqrt(b * c * p * (p - a));
-    return bisection;
+float getTriangleBisection(float a, float b, float c, float semiPerimeter) {
+    return (2 / (b + c)) * sqrt(b * c * semiPerimeter * (semiPerimeter - a));
 }
 void outputTriangleParameters(float a, float b, float c) {
     float area, perimeter,
@@ -59,43 +56,38 @@ void clearInput() {
 }
 float onTriangleSideValue() {
     float sideValue = 0;
-    const float maxFloatNumber = 3.4e38;
     if (scanf("%f", &sideValue) != 1) {
         printf("Error: Invalid value, please enter a valid number\n");
         clearInput();
-        return 0;
-    }
-    if (sideValue == 0) {
-        printf("Program stopped\n");
         return -1;
+    }
+    if (!sideValue) {
+        printf("Program stopped\n");
+        return 0;
     } else if (sideValue < 0) {
         printf("Error: Number must be positive only\n");
-        return 0;
-    } else if (sideValue > maxFloatNumber) {
-        printf("Error: Number must be less than %f\n", maxFloatNumber);
-        return 0;
+        return -1;
+    } else if (sideValue > FLT_MAX) {
+        printf("Error: Number must be less than %e\n", FLT_MAX);
+        return -1;
     }
     return sideValue;
 }
-int inputTriangleSides(float* a, float* b, float* c) {
+bool inputTriangleSides(float* a, float* b, float* c) {
     struct object triangleSides[] = {
         {'a', 0},
         {'b', 0},
         {'c', 0},
     };
-    int triangleSidesLength = 0;
-    if (sizeof(triangleSides) > 0) {
-        triangleSidesLength = sizeof(triangleSides) / sizeof(triangleSides[0]);
-    } else {
-        return 0;
-    }
+    size_t totalSize = sizeof(triangleSides);
+    int triangleSidesLength = totalSize ? totalSize / sizeof(*triangleSides) : totalSize;
     for (int i = 0; i < triangleSidesLength; ++i) {
-       float side = 0; 
-        while (side == 0) {
+       float side = -1; 
+        while (side == -1) {
             printf(" Input side %c: ", triangleSides[i].key);
             side = onTriangleSideValue();
-            if (side == -1) {
-                return -1;
+            if (!side) {
+                return false;
             }
             triangleSides[i].value = side;
         }
@@ -103,37 +95,31 @@ int inputTriangleSides(float* a, float* b, float* c) {
     *a = triangleSides[0].value;
     *b = triangleSides[1].value;
     *c = triangleSides[2].value;
-    return 1;
+    return true;
 }
-int isThisTriangleExists(float a, float b, float c) {
-    if (a + b <= c || a + c <= b || b + c <= a) {
-        printf("Error: This triangle does not exist (sum of two sides should be more than third side)\n");
-        return 0;
-    }
-    return 1;
+bool isTriangleValid(float a, float b, float c) {
+    return a + b > c && a + c > b && b + c > a;
 }
-int startProgram() {
+bool startProgram() {
     float a, b, c = 0; // a, b, c - triangle sides
 
-    printf("\nInput triangle sides (from 0 to 3.4e38) to get triangle parameters. Input 0 to stop program\n");
-    if (inputTriangleSides(&a, &b, &c) == -1) {
-        return -1;
+    printf("\nInput triangle sides (from %.0f to %.1e) to get triangle parameters. Input 0 to stop program\n",
+    FLT_EPSILON, FLT_MAX);
+    if (!inputTriangleSides(&a, &b, &c)) {
+        return false;
     }
 
-    if (isThisTriangleExists(a, b, c) == 0) {
-        return 0;
+    if (!isTriangleValid(a, b, c)) {
+        printf("Error: This triangle does not exist (sum of two sides should be more than third side)\n");
+        return true;
     }
 
     printf("\nTriangle Parameters\n");
     outputTriangleParameters(a, b, c);
 
-    return 0;
+    return true;
 }
 int main() {
-    while (1) {
-        if (startProgram() == -1) {
-            break;
-        }
-    }
+    while (startProgram()) {}
     return 0;
 }
